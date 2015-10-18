1c3ec9b8355e8d06cfb6f1333c6d4c16acdbde79
Aaron Gamble <aaronsgamble@gmail.com>
2015-10-18 10:56:54 +0100

    Added getTags() - Print out tags for million song subset from Lastfm database

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String args[]){
-        Lyrics l = new Lyrics();
-        l.getLyrics();
+//        Lyrics l = new Lyrics();
+//        l.getLyrics();
+
+        Tags t = new Tags();
+        t.getTags();
     }
 }
